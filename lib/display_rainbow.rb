6928d8cf9aea060be1f1b7b8db5d738c5780b2e4
Joshua James <burke.joshua.james@gmail.com>
2017-09-08 23:51:14 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,5 +3,5 @@ def display_rainbow(colors)
   result = colors.each_with_object([]).with_index do |(elem, array), index|
     array << "#{colors[index][0].capitalize}: #{elem}"
   end
-  puts results.join(', ')
+  puts result.join(', ')
 end
