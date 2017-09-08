616bf331dc868aa41a828763062dfd364abdeef3
Joshua James <burke.joshua.james@gmail.com>
2017-09-08 23:44:37 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   result = colors.each_with_object("").with_index do |(_, i), string|
+    puts i
     match = [colors[i].capitalize[0], colors[i]]
     string << "#{match.first}: #{match.last}"
     puts match
