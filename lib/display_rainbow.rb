36eeb1a4e6c048a114a7c4da3906e78c4c2cb8f7
Joshua James <burke.joshua.james@gmail.com>
2017-09-08 23:49:31 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   result = colors.each_with_object("").with_index do |(elem, string), index|
-    puts "String: #{string}"
-    puts "elem: #{elem}"
-    puts "index: #{index}"
     string << "#{colors[index][0].capitalize}: #{elem}, "
-    puts string
   end
   puts result
 end
