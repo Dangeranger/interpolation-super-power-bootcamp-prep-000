46760c23588a4d817ea3bab8f4dfbfac6315df8a
Joshua James <burke.joshua.james@gmail.com>
2017-09-08 23:35:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   colors.each_with_index do |e, i|
-    hash = { colors[i].capitalize.first => colors[i] }
+    hash = { colors[i].capitalize[0] => colors[i] }
     puts "#{hash.key}: #{hash.value}"
   end
 end
